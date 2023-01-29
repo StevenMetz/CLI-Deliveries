74a4b644c2038847bb866232a432f20f182c994a
StevenMetz <stevenm.codes@gmail.com>
2023-01-28 18:55:37 -0600

    FINISHED

--- Thoughts.md
+++ Thoughts.md
@@ -1,24 +1,37 @@
 ### PsuedoCode
 
-This CLI should be able to take as arguments two files one with the input the the names for the drivers and the next argument the file containing the addresses. Then we need to sort each of the arrays into easier to work with data. This names will be moved into arrays of vowels and consonants checking which name has more vowels or consonants and putting them into their perspective place and sorting them by greatest to least. In doing this to the names we can now easily make use of our SS for the addresses to be delivered to. Now its time to sort through the addresses and move them into seperate arrays by the length of the addresses one array being odd the other even. We now have the data sorted in a way that makes doing the math for the SS easy for us. Next the methods for calculating our ss
+This CLI should be able to take as arguments two files one with the input the the names for the drivers and the next argument the file containing the addresses. Then we need to sort each of the arrays into easier to work with data. These names will be moved into arrays of vowels and consonants checking which name has more vowels or consonants and putting them into their perspective place and sorting them by greatest to least. In doing this to the names we can now easily make use of our SS for the addresses to be delivered to. Now its time to sort through the addresses and move them into seperate arrays by the length of the addresses one array being odd the other even. We now have the data sorted in a way that makes doing the math for the SS easy for us. Next the methods for calculating our ss
 need to be created working with vowels for even streets and working with consonants for odd streets. Even streets have the greatest chance of giving us the highest SS score so we will itterate through those first. Since we are working with evens and odds the bonus SS will be added to each one if the the name is also even or odd. Even streets with even names should give the highest SS. Until even addresses array is empty we will use that array first since odd addresses with odd names are the same as even addresses. Then odd names with odd addresses will be will done next after odd names are finished the program will finish off assigning the names to addresses in order they come.
 
-### First
+### Steps Taken
 
-Write logic for taking in users input for names and put those names into an array
-
-### Second
-
-Write the logic to take in the address to be delivered to
-
-### Fourth
-
-Sort through array of addresses and determine which ones are even and odd and then move them to new arrays that include only odd and even addresses
-
-### Third
-
-Make Methods to determine the SS in a seperate file inside folder
-
-### sixth
-
-split the names array into two one being of vowels and on being of consonants based on checking which is greater the vowels or consonants and ones that are equal will be moved to vowels with the greater chance of exploiting the SS. this will be done until even of vowels are the same as even streets or until there are names with vowels contain less than the
+1. Write methods to do the math for SS
+2. Test method work with printing to terminal
+3. Write methods to split names and addresses to evens and odds
+4. Test methods work with printing to terminal
+5. Write methods to sort even names by vowels and odd names by consonants
+6. Test methods work with printing to terminal
+7. Write methods for taking in two seperate files as arguments
+8. Print statements to the terminal asking for names and addresses
+9. Write logic for getting the users input for both names and addresses
+10. Test that the input works by printing to the terminal the data entered
+11. Split up the names and addresses into even and odd arrays to maximize ss
+12. Test that it works as intended with printing to terminal
+13. Write a loop that iterates through the even names and pairing them with even addresses and gives SS
+14. Test functionality of loop
+15. Write loop that iterates through odd names and odd addresses and gives SS
+16. Make Variable called names_left
+17. Write logic that checks to see if even names are empty if not move them to names left
+18. Test that the logic acts as intended
+19. Repeat #17 for odd name
+20. Write the logic for checking if even_addresses are empty if not loop through even addresses pairing remaing names
+21. Test #20 works with printing to terminal
+22. Do same as 20 but for odd addresses instead
+23. Test 22 works with printing to terminal
+24. Put ss keys together with names and addresses with variable
+25. Test 24 works with printing to terminal
+26. Sum up ss keys array
+27. Test 26 works with printing to terminal
+28. Print variable from 24 to terminal
+29. Print variable from 26 to terminal
+30. Make a Read Me
--- methods.rb
+++ methods.rb
@@ -1,93 +1,130 @@
 ### Create Method to count vowels inside drivers name when street is even
 def even_street(name)
-  vowels = 0
-  index = 0
-  while index < name.length
+  vowels = 0 # declare vowels set to 0
+  index = 0 # declare index set to 0
+  name = name.gsub(/\s+/, "") # removes all white space from names for accurate character matching
+
+  while index < name.length # loops through names
     if name[index] =~ /[aeiou]/ # uses regex to determine if string index is a vowel
-      vowels += 1
+      vowels += 1 # increases vowels count by 1
     end
-    index += 1
+    index += 1 # increases index by 1
   end
-  @ss_vowels = vowels * 1.5
-  return @ss_vowels
+  @ss_vowels = vowels * 1.5 # math for SS
+  return @ss_vowels # returns variable and makes it available for through out program
 end
 
 # for odd street length
-def odd_street(name)
-  @consts = 0
-  index = 0
-  while index < name.length
-    if name[index] !~ /[aeiou]/ # uses regex to determin if string index is not a vowel
-      @consts += 1
+def odd_street(names)
+  consts = 0 # declare consonants short handed to 0
+  index = 0 # declare index set to 0
+  names = names.gsub(/\s+/, "") # removes all white space from names for accurate character matching
+  while index < names.length
+    if names[index] !~ /[aeiou]/ # uses regex to determin if string index is not a vowel
+      consts += 1 # increases consts count by 1
     end
-    index += 1
+    index += 1 #increases index by 1
   end
-  return @consts  #only returns consts because ss is multiplied * 1
-end
-
-def vowels?(name)
-  vowels = 0
-  index = 0
-  while index < name.length
-    if name[index] =~ /[aeiou]/ # uses regex to determine if string index is a vowel
-      vowels += 1
-    end
-    index += 1
-  end
-  return vowels
-end
-
-def consts?(name)
-  consts = 0
-  index = 0
-  while index < name.length
-    if name[index] !~ /[aeiou]/ # uses regex to determin if string index is not a vowel
-      consts += 1
-    end
-    index += 1
-  end
-  return consts
+  return consts  #only returns consts because ss is multiplied * 1
 end
 
 #puts the addresses into even and odd addresses
 
 def make_two_new_arrays_for_addresses(addresses)
-  @odd_address = []
-  @even_address = []
-  addresses.each do |address|
+  @odd_address = [] # empty array for odd addresses
+  @even_address = [] # empty array for even addresses
+  addresses.each do |address| #loops through addresses by each address
+    # removes all white space from names for accurate length and checks if length is odd
     if address.gsub(/\s+/, "").length % 2 != 0
-      @odd_address << address
+      @odd_address << address # moves odd address to odd addresses
     else
-      @even_address << address
+      @even_address << address # moves all addresses that weren't odd to even addresses
     end
   end
+  # sorts addresses by length
+  @odd_address = sort_by_length(@odd_address)
+  @even_address = sort_by_length(@even_address)
+end
+
+# sort addresses by length greatest to least
+def sort_by_length(addresses)
+  addresses.sort_by! { |str| str.length }.reverse
 end
 
 # sorts array by the vowels
 
 def sort_by_vowels(names)
-  names.sort_by! { |str| str.count("aeiouAEIOU") }
-  @names_vowels = names.reverse
+  names.sort_by! { |str| str.count("aeiouAEIOU") } # does the sorting from least to greatest
+  @names_vowels = names.reverse # sorts the names by greatest amount vowels
   return @names_vowels
 end
 
 #sorts names by their consonants
 def sort_by_consonants(names)
   names.sort_by! { |str| str.scan(/[^aeiou]/).size }
-  @names_cons = names.reverse
+  @names_cons = names.reverse # sorts the names by greatest amount of consonants to least
   return @names_cons
 end
 
 # split the names to evens and odds
 
 def split_names_to_evens_and_odds(names)
-  @even_names = []
-  @odd_names = []
-  names.each do |name|
-    if name.gsub(/\s+/, "").length % 2 == 0
-      @even_names << name
+  @even_names = [] # empty array for even names
+  @odd_names = [] # empty array for odd names
+  names.each do |name| #loops through names by each name
+    if name.gsub(/\s+/, "").length % 2 == 0 # checks if name is even and removes white space to only count characters
+      @even_names << name # moves names to even names
     else
-      @odd_names << name
+      @odd_names << name  # moves other names to odd names
     end
   end
 end
+
+# counts just consonants for comparison
+def consts?(name)
+  consts = 0 # sets const to 0
+  index = 0 # declare index set to 0
+  name = name.gsub(/\s+/, "") # removes white space for accurate consonant matching
+  while index < name.length #loops through names while index is less than length
+    if name[index] !~ /[aeiou]/ # uses regex to determin if string index is not a vowel
+      consts += 1 # increases consts by 1
+    end
+    index += 1 # increases index by 1
+  end
+  return consts #returns the consts count
+end
+
+# counts all the vowels in name for comparison
+def vowels?(name)
+  vowels = 0 # declare vowels set to 0
+  index = 0 # declare index set to 0
+  name = name.gsub(/\s+/, "") # removes white space for accurate vowel matching
+
+  while index < name.length # loops through names while index is less than length
+    if name[index] =~ /[aeiou]/ # uses regex to determine if string index is a vowel
+      vowels += 1 # increase vowels by 1
+    end
+    index += 1 # increases index by 1
+  end
+end
+
+# Function to read names from a CSV file
+def read_names(file)
+  @names = []
+  CSV.foreach(file) do |row|
+    @names << row[0].downcase.strip
+  end
+  @names
+end
+
+# Function to read addresses from a CSV file
+def read_addresses(file)
+  addresses = []
+  CSV.foreach(file) do |row|
+    addresses << row[0].downcase.strip
+  end
+  addresses
+end
+
+name = ["odd", "odd"]
+p odd_street(name[0])
